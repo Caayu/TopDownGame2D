719da70d5bb682716a81366d1c0fe6f6abd1b6be
Caayu <henrycay@outlook.com>
2024-04-28 21:13:27 -0300

    player movement animations

--- Objects/Player.gd
+++ Objects/Player.gd
@@ -2,6 +2,12 @@ extends CharacterBody2D
 
 
 const SPEED = 60.0
+var dir_name = "side_"
+
+@onready var animated_sprite : AnimatedSprite2D = $AnimatedSprite2D
+
+func _ready():
+	animated_sprite.play("front_idle")
 
 # Sempre lide com movimento aqui:
 func _physics_process(delta):
@@ -13,3 +19,31 @@ func _physics_process(delta):
 	velocity = velocity.normalized() * SPEED
 	
 	move_and_slide()
+	
+func _process(delta):
+	var anim_name = "idle"
+
+	if Input.is_action_just_pressed("attack"):
+		print("atacou!")
+	
+	if velocity.length() > 0:
+		anim_name = "walk"
+		
+		if velocity.x != 0:
+			dir_name = "side_"
+		elif velocity.y > 0:
+			dir_name = "front_"
+		elif velocity.y < 0:
+			dir_name = "back_"
+
+		animated_sprite.flip_h = dir_name == "side_" and velocity.x < 0
+
+	animated_sprite.play(dir_name + anim_name)
+	
+	
+	
+	
+	
+	
+	
+	
